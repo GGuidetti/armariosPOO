7b1b8f47447968ff4b46ef422b548831082807c1
Luis Fernando Brasil <luis4756@live.com>
2022-11-27 20:43:46 -0300

    Implementação CRUD Estudantes

--- src/main/java/br/edu/ifpr/paranavai/armarios/visao/EditorEstudantesUI.java
+++ src/main/java/br/edu/ifpr/paranavai/armarios/visao/EditorEstudantesUI.java
@@ -6,7 +6,9 @@ package br.edu.ifpr.paranavai.armarios.visao;
 
 import br.edu.ifpr.paranavai.armarios.controle.EstudanteControle;
 import br.edu.ifpr.paranavai.armarios.modelo.Estudante;
+import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -21,10 +23,29 @@ public class EditorEstudantesUI extends javax.swing.JFrame {
      */
     public EditorEstudantesUI() {
         initComponents();
+        
+        this.setLocationRelativeTo(null);
         this.listaEstudante = EstudanteControle.listarTodosEstudantes();
-        populaCorreto(this.listaEstudante);
+        populaTabela(listaEstudante);
     }
-
+    
+    private void populaTabela(List<Estudante> lista) {
+        DefaultTableModel modeloDeColunasDaTabela = (DefaultTableModel) tblEstudantes.getModel();
+        //  Primeiro limpa a tabela
+        while (modeloDeColunasDaTabela.getRowCount() != 0) {
+            modeloDeColunasDaTabela.removeRow(0);
+        }
+        
+        for (int i = 0; i < lista.size(); i++) {
+            Estudante mostraEstudante = lista.get(i);
+            Object[] dadosLinha = new Object[4];
+            dadosLinha[0] = mostraEstudante.getId();
+            dadosLinha[1] = mostraEstudante.getNome();
+            dadosLinha[2] = mostraEstudante.getEmail();
+            dadosLinha[3] = mostraEstudante.getTelefone();
+            modeloDeColunasDaTabela.addRow(dadosLinha);
+        }
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -37,6 +58,14 @@ public class EditorEstudantesUI extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tblEstudantes = new javax.swing.JTable();
         jPanel1 = new javax.swing.JPanel();
+        panelNovo = new javax.swing.JPanel();
+        btnNovo = new javax.swing.JButton();
+        btnEditar = new javax.swing.JButton();
+        btnExcluir = new javax.swing.JButton();
+        panelBusca = new javax.swing.JPanel();
+        lblNome = new javax.swing.JLabel();
+        txtNome = new javax.swing.JTextField();
+        btnBuscar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -75,35 +104,154 @@ public class EditorEstudantesUI extends javax.swing.JFrame {
             tblEstudantes.getColumnModel().getColumn(3).setResizable(false);
         }
 
+        panelNovo.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panelNovo.setLayout(new java.awt.GridLayout(1, 3, 10, 0));
+
+        btnNovo.setText("Novo");
+        btnNovo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnNovoActionPerformed(evt);
+            }
+        });
+        panelNovo.add(btnNovo);
+
+        btnEditar.setText("Editar");
+        btnEditar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEditarActionPerformed(evt);
+            }
+        });
+        panelNovo.add(btnEditar);
+
+        btnExcluir.setText("Excluir");
+        btnExcluir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExcluirActionPerformed(evt);
+            }
+        });
+        panelNovo.add(btnExcluir);
+
+        panelBusca.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 0, 10));
+        panelBusca.setLayout(new javax.swing.BoxLayout(panelBusca, javax.swing.BoxLayout.LINE_AXIS));
+
+        lblNome.setText("Nome: ");
+        panelBusca.add(lblNome);
+
+        txtNome.setSelectedTextColor(new java.awt.Color(204, 204, 204));
+        txtNome.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNomeActionPerformed(evt);
+            }
+        });
+        panelBusca.add(txtNome);
+
+        btnBuscar.setText("Buscar");
+        btnBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBuscarActionPerformed(evt);
+            }
+        });
+        panelBusca.add(btnBuscar);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(panelNovo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(panelBusca, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(12, Short.MAX_VALUE)
+                .addComponent(panelBusca, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelNovo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 593, Short.MAX_VALUE)
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 593, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 303, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 303, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnNovoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNovoActionPerformed
+        FormEstudanteUI form = new FormEstudanteUI();
+        this.setVisible(false);
+        form.setLocationRelativeTo(this);
+        form.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btnNovoActionPerformed
+
+    private void btnEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditarActionPerformed
+        if (tblEstudantes.getSelectedRow() >= 0) {
+            int dadosLinha = tblEstudantes.getSelectedRow();
+            int codigo = (int) tblEstudantes.getModel().getValueAt(dadosLinha, 0);
+            FormEstudanteUI form = new FormEstudanteUI(codigo);
+            this.setVisible(false);
+            form.setLocationRelativeTo(this);
+            form.setSize(500,440);
+            form.setVisible(true);
+            dispose();
+        }else {
+            JOptionPane.showMessageDialog(null, "Selecione um Estudante!");
+        }
+    }//GEN-LAST:event_btnEditarActionPerformed
+
+    private void btnExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExcluirActionPerformed
+        if (tblEstudantes.getSelectedRow() >= 0) {
+            int resposta = JOptionPane.showConfirmDialog(null, "Confirma a exclusão do Estudante?", "Excluir Estudante!", JOptionPane.YES_NO_OPTION);
+
+            if (resposta == 0) {
+                int dadosLinha = tblEstudantes.getSelectedRow();
+                int codigo = (int) tblEstudantes.getModel().getValueAt(dadosLinha, 0);
+
+                for (Estudante estudante : this.listaEstudante) {
+                    if(estudante.getId()== codigo)
+                    EstudanteControle.excluir(estudante);
+                }
+
+                this.listaEstudante = EstudanteControle.listarTodosEstudantes();
+
+                populaTabela(this.listaEstudante);
+
+                JOptionPane.showMessageDialog(null, "Estudante Removida!");
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Selecione um Estudante!");
+        }
+    }//GEN-LAST:event_btnExcluirActionPerformed
+
+    private void txtNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNomeActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtNomeActionPerformed
+
+    private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
+        ArrayList<Estudante> filtrado = new ArrayList<Estudante>();
+
+        for (Estudante estudante : listaEstudante) {
+            if(estudante.getNome().toUpperCase().contains(txtNome.getText().toUpperCase()))
+            filtrado.add(estudante);
+        }
+
+        populaTabela(filtrado);
+    }//GEN-LAST:event_btnBuscarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -142,24 +290,16 @@ public class EditorEstudantesUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnBuscar;
+    private javax.swing.JButton btnEditar;
+    private javax.swing.JButton btnExcluir;
+    private javax.swing.JButton btnNovo;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblNome;
+    private javax.swing.JPanel panelBusca;
+    private javax.swing.JPanel panelNovo;
     private javax.swing.JTable tblEstudantes;
+    private javax.swing.JTextField txtNome;
     // End of variables declaration//GEN-END:variables
-    private void populaCorreto(List<Estudante> listaEstudante) {
-        DefaultTableModel modeloDeColunasDaTabela = (DefaultTableModel) tblEstudantes.getModel();
-        
-        while (modeloDeColunasDaTabela.getRowCount() != 0) {
-            modeloDeColunasDaTabela.removeRow(0);
-        }
-        
-        for (Estudante estudante : listaEstudante) {
-            Object[] dadosLinha = new Object[4];
-            dadosLinha[0] = estudante.getRa();
-            dadosLinha[1] = estudante.getNome();
-            dadosLinha[2] = estudante.getEmail();
-            dadosLinha[3] = estudante.getTelefone();
-            modeloDeColunasDaTabela.addRow(dadosLinha);
-        }
-    }
 }
